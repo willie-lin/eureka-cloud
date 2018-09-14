3b9807361b9c411f21c55c99027c748ccdd15114
yuan <yyhmmwan@163.com>
2018-09-14 09:35:36 +0800

    Initial commit

---
+++ eureka-server/src/main/java/com/eureka/cloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.eureka.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author YuAn
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
