bb2f1f58e18dadafed2aa51408fc1926fa124928
yuan <yyhmmwan@163.com>
2018-09-14 18:34:44 +0800

    test eureka eureka-feign

---
+++ eureka-feign/src/main/java/com/eureka/cloud/eurekafeign/EurekaFeignApplication.java
@@ -0,0 +1,15 @@
+package com.eureka.cloud.eurekafeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients
+@SpringBootApplication
+public class EurekaFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaFeignApplication.class, args);
+    }
+}
