9e734539060f61ca563cb349f4ebd9e5e8d770bb
yuan <yyhmmwan@163.com>
2018-09-14 12:29:25 +0800

    delete eureka-hi add eureka-client

---
+++ eureka-client/src/main/java/com/eureka/cloud/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package com.eureka.cloud.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author YuAn
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
---
+++ eureka-client/src/main/java/com/eureka/cloud/eurekaclient/HelloController.java
@@ -0,0 +1,30 @@
+package com.eureka.cloud.eurekaclient;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author YuAn
+ * @Package: com.eureka.cloud.eurekahi
+ * @auther: YuAn
+ * @Date: 2018/9/13
+ * @Time: 22:16
+ * @Project_name: eureka-cloud
+ * To change this template use File | Settings | File Templates.
+ * @Description:
+ */
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name) {
+        return "Hello, " + name + " " + new Date();
+    }
+
+}
