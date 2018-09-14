259f412e2769762d05434f085e7867c55be42d39
yuan <yyhmmwan@163.com>
2018-09-14 12:40:48 +0800

    update eureka-client

--- eureka-client/src/main/java/com/eureka/cloud/eurekaclient/controller/HelloController.java
+++ eureka-client/src/main/java/com/eureka/cloud/eurekaclient/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.eureka.cloud.eurekaclient;
+package com.eureka.cloud.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
