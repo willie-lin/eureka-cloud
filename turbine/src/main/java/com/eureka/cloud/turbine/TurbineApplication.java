77649c4601460cd28a4e9121ba870e4869838035
yuan <yyhmmwan@163.com>
2018-09-27 21:38:58 +0800

    add turbine

--- turbine/src/main/java/com/eureka/cloud/turbine/TurbineApplication.java
+++ turbine/src/main/java/com/eureka/cloud/turbine/TurbineApplication.java
@@ -2,7 +2,9 @@ package com.eureka.cloud.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+@EnableTurbine
 @SpringBootApplication
 public class TurbineApplication {
 
