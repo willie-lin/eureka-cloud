620a0ada8ab8044bf46281bc3fbacf41fbb7dff2
yuan <yyhmmwan@163.com>
2018-09-27 10:37:14 +0800

    add Hystrix Feign and Hystrix-Dashboard

---
+++ eureka-feign/src/main/java/com/eureka/cloud/eurekafeign/interfaces/HelloRemoteHystrix.java
@@ -0,0 +1,25 @@
+package com.eureka.cloud.eurekafeign.interfaces;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author YuAn
+ * @Package: com.eureka.cloud.eurekafeign.interfaces
+ * @auther: YuAn
+ * @Date: 2018/9/18
+ * @Time: 19:59
+ * @Project_name: eureka-cloud
+ * To change this template use File | Settings | File Templates.
+ * @Description:
+ */
+
+@Component
+public class HelloRemoteHystrix implements HelloRemote {
+    @Override
+    public String hello(@RequestParam(value = "name") String name) {
+        return "hello world!";
+    }
+}
