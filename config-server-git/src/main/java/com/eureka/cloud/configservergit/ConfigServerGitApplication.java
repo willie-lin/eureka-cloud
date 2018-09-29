f75c015bb81066b4b661b2f1a970c8a504438c11
yuan <yyhmmwan@163.com>
2018-09-29 17:29:50 +0800

    add spring boot config

---
+++ config-server-git/src/main/java/com/eureka/cloud/configservergit/ConfigServerGitApplication.java
@@ -0,0 +1,17 @@
+package com.eureka.cloud.configservergit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author YuAn
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerGitApplication.class, args);
+    }
+}
---
+++ config-server-git/src/test/java/com/eureka/cloud/configservergit/ConfigServerGitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eureka.cloud.configservergit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigServerGitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
