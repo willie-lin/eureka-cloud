d52039a06d294eb0faa31871cb1d0dc2646c8c48
yuan <yyhmmwan@163.com>
2018-09-29 21:03:13 +0800

    add spring boot config-client

---
+++ config-client/src/main/java/com/eureka/cloud/configclient/controller/HelloController.java
@@ -0,0 +1,40 @@
+package com.eureka.cloud.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author YuAn
+ * @Package: com.eureka.cloud.configclient.controller
+ * @auther: YuAn
+ * @Date: 2018/9/29
+ * @Time: 18:56
+ * @Project_name: eureka-cloud
+ * To change this template use File | Settings | File Templates.
+ * @Description:
+ */
+
+@RestController
+public class HelloController {
+
+//    @Value("${info.profile:error}")
+//        private String profile;
+//
+//        @GetMapping("/info")
+//        public Mono<String> hello() {
+//            return Mono.justOrEmpty(profile);
+//    }
+
+    @Value("${info.profile:error}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
