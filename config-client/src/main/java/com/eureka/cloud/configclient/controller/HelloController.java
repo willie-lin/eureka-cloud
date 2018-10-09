e96c4717d1fdbb5c18b1954caf506df7b626280e
yuan <yyhmmwan@163.com>
2018-10-09 21:25:37 +0800

    Transform the configuration center to achieve service and high availability

--- config-client/src/main/java/com/eureka/cloud/configclient/controller/HelloController.java
+++ config-client/src/main/java/com/eureka/cloud/configclient/controller/HelloController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
